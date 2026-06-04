62ca006cf41ac0c129f840b4f46c84a84b7bdb51
Geratulcong <gerolopez115@gmail.com>
2026-06-03 22:42:56 -0400

    Update setup_ble_service.py

--- Codigos_raspberry/services/setup_ble_service.py
+++ Codigos_raspberry/services/setup_ble_service.py
@@ -1,5 +1,11 @@
+import asyncio
+
 class SetupBLEService:
 
     async def iniciar(self):
 
-        print("Modo configuración BLE")
+        print("Modo configuración BLE iniciado")
+
+        while True:
+            print("Esperando datos desde la página...")
+            await asyncio.sleep(5)
